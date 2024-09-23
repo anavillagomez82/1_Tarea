0bcaae45f65a6d6e1a104c97612baf46aceed518
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-09-23 18:22:04 -0500

    Add files via upload

---
+++ ejercicio 2.17.cpp
@@ -0,0 +1,22 @@
+//a)
+#include <iostream>
+
+int main() {
+  std::cout << "V X " << "Y Z" << std::endl;
+  return 0;
+}
+//b)
+#include <iostream>
+
+int main() {
+  std::cout << "V " << "X " << "Y " << "Z" << std::endl;
+  return 0;
+}
+//c)
+#include <iostream>
+
+int main() {
+  std::cout << "V X ";
+  std::cout << "Y Z" << std::endl;
+  return 0;
+}
