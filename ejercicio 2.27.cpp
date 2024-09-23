9427cdc3ea4ae8f26960064f71bbae73300346c7
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-09-23 18:25:20 -0500

    Add files via upload

---
+++ ejercicio 2.27.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "A"; // Imprime la letra A en la consola
+  cout << static_cast<int>('A'); // Imprime el valor entero ASCII del caracter 'A' en la consola
+  return 0;
+}
