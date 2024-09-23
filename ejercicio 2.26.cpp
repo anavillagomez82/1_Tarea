3c7aaba2e387eea48402e2428ffd9dd7fb6783ba
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-09-23 18:25:04 -0500

    Add files via upload

---
+++ ejercicio 2.26.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // 8 lineas
+  cout << "*****" << endl;
+  cout << "****" << endl;
+  cout << "**" << endl;
+  cout << "**" << endl;
+  cout << "*****" << endl;
+  cout << "*" << endl;
+  cout << "****" << endl;
+  cout << "*" << endl;
+
+  // Menos lineas
+  cout << "*****" << endl;
+  cout << "****" << endl;
+  cout << "**" << endl;
+  cout << "**" << endl << "*****" << endl;
+  cout << "*\n****\n*" << endl;
+
+  return 0;
+}
